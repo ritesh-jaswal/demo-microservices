37e52b3935a3fa96cd0e1a4d6cb7b32a217f0508
ritesh <ritesh.jaswal@testingxperts.com>
2024-09-17 17:43:56 +0530

    TRYING TO ADD CIRCUIT BREAKER WITH RESILIENT4J

--- UserService/src/main/java/com/project/user/service/UserService/controllers/UserController.java
+++ UserService/src/main/java/com/project/user/service/UserService/controllers/UserController.java
@@ -2,11 +2,15 @@ package com.project.user.service.UserService.controllers;
 
 import com.project.user.service.UserService.entities.User;
 import com.project.user.service.UserService.services.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -16,26 +20,59 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     //create
     @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@RequestBody User user)
+    {
         User user1 = userService.saveUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(user1);
     }
 
-    //single user get
     @GetMapping("/{userId}")
+    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getSingleUser(@PathVariable String userId)
     {
+        logger.debug("Attempting to retrieve user with ID: {}", userId);
         User user = userService.getUser(userId);
         return ResponseEntity.ok(user);
     }
 
-    //all user get
-    @GetMapping
-    public ResponseEntity<List<User>> getAllUser()
+    public ResponseEntity<User> ratingHotelFallback(String userId, Throwable ex)
     {
+        logger.info("Fallback is executed because service is down : {}", ex.getMessage());
+        ex.printStackTrace();
+
+        User user = User.builder()
+                .email("dummy@gmail.com")
+                .name("Dummy")
+                .about("This user is created dummy because some service is down")
+                .userId("141234").build();
+
+        return new ResponseEntity<>(user, HttpStatus.SERVICE_UNAVAILABLE);
+    }
+
+    @GetMapping
+    @CircuitBreaker(name = "allRatingHotelBreaker", fallbackMethod = "allRatingHotelFallback")
+    public ResponseEntity<List<User>> getAllUser() {
         List<User> allUser = userService.getAllUser();
         return ResponseEntity.ok(allUser);
+    }
+
+    public ResponseEntity<List<User>> allRatingHotelFallback(Throwable ex) {
+        logger.info("Fallback is executed because some service is down : {}", ex.getMessage());
+        ex.printStackTrace();
+
+        User user = User.builder()
+                .email("dummy@gmail.com")
+                .name("Dummy")
+                .about("This user is created dummy because some service is down")
+                .userId("141234").build();
+
+        List<User> users = new ArrayList<>();
+        users.add(user);
+
+        return new ResponseEntity<>(users, HttpStatus.SERVICE_UNAVAILABLE);
     }
 }
--- UserService/src/main/java/com/project/user/service/UserService/exceptions/GlobalExceptionHandler.java
+++ UserService/src/main/java/com/project/user/service/UserService/exceptions/GlobalExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<ApiResponse> handleResourceNotFoundException(ResourceNotFoundException ex) {
@@ -16,9 +17,17 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
+    public ResponseEntity<ApiResponse> handleRuntimeException(RuntimeException ex) {
+        ApiResponse response = new ApiResponse("Service is currently unavailable: " + ex.getMessage(), false);
+        return new ResponseEntity<>(response, HttpStatus.SERVICE_UNAVAILABLE);
+    }
+
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<ApiResponse> handleGenericException(Exception ex) {
+        // Log the exception here if needed
         ApiResponse response = new ApiResponse("An unexpected error occurred.", false);
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
